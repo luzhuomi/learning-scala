62ac7cbcb9cf9d063e6cd5b65856a141243ca004
Kenny Lu <luzhuomi@gmail.com>
2016-06-03 13:49:25 +0800

    update

--- examples/oop/OOP.scala
+++ examples/oop/OOP.scala
@@ -7,6 +7,9 @@ class Person(n:String,i:String) {
 	def getId():String = id
 }
 
+trait NightOwl {
+	def stayUpLate():Unit 
+}
 
 class Student(n:String, i:String, g:Double) extends Person(n,i) with NightOwl {
 	private var gpa = g
@@ -30,31 +33,21 @@ class Staff(n:String, i:String, sal:Double) extends Person(n,i) {
 	}
 }
 
-trait NightOwl {
-	def stayUpLate():Unit 
-}
 
 /*
-object OOP
-{
-	
+scala> :load OOP.scala
+Loading OOP.scala...
+defined class Person
+defined trait NightOwl
+defined class Student
+defined class Staff
 
-}
-*/
-/*
-class Rational(n: Int, d: Int) {
-	private def gcd(x: Int, y: Int): Int = {
-		if (x == 0) y
-		else if (x < 0) gcd(-x, y)
-		else if (y < 0) -gcd(x, -y)
-		else gcd(y % x, x)
-	}
-	private val g = gcd(n, d)
-	val numer: Int = n/g
-	val denom: Int = d/g
-	def +(that: Rational) = new Rational(numer * that.denom + that.numer * denom,denom * that.denom)
-	def -(that: Rational) =	new Rational(numer * that.denom - that.numer * denom,denom * that.denom)
-	def *(that: Rational) =	new Rational(numer * that.numer, denom * that.denom)
-	def /(that: Rational) =	new Rational(numer * that.denom, denom * that.numer)
-}
+scala> val tom = new Student("Tom", "X1235", 4.0)
+tom: Student = Student@601c1dfc
+
+scala> val martin = new Staff("Martin", "T0001", 500000.0)
+martin: Staff = Staff@650fbe32
+
+scala> tom.stayUpLate
+woohoo
 */
