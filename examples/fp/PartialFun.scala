9f1cf51dbb884ae14e751004725438f853e3b091
Kenny Lu <luzhuomi@gmail.com>
2016-06-14 13:46:30 +0800

    updated

--- examples/fp/PartialFun.scala
+++ examples/fp/PartialFun.scala
@@ -2,7 +2,10 @@ object PartialFun {
 	def div(x:Int): PartialFunction[Int,Int] = {
 		case (y:Int) if y != 0 => x/y
 	}
-	// val x = div(2)(add(1)(mult(3)(0))) // 2
-	// val y = div(2)(add(0)(mult(3)(0))) // this will make the partial function ill defined
 	val xs = List(0,1,2) collect ( div(2) ) // List(2, 1)
+	// val ys = List(0,1,2) map ( div(2) ) // we have a scala.MatchError
+	val mkZero: PartialFunction[Int,Int] = {
+		case (y:Int) if y == 0 => 0
+	}
+	val ys = List(0,1,2) map ( div(2) orElse mkZero ) // List(0, 2, 1)
 }
